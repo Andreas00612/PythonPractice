1a8560dbc7cbb3335a1f740aa9f150e4e42828e3
Andreas00612 <113928052+Andreas00612@users.noreply.github.com>
2022-10-26 14:44:26 +0800

    Update main_3.py

--- 1024/main_3.py
+++ 1024/main_3.py
@@ -1,4 +1,4 @@
-###102403####
+###102403<sol1>####
 class ListNode:
     def __init__(self, data):
         self.data = data
@@ -46,6 +46,65 @@ class DoubleLinkedList:
                 current.prev = None
             else:
                 return
+
+with open('input.pkl','rb') as inp:
+    linkedlist = pickle.load(inp)
+    linkedlist.traverse()
+    
+    
+    
+####102403<sol2>####
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+        return
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def add_list_item(self, item):
+        if not isinstance(item, ListNode):
+            item = ListNode(item)
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+            self.tail = item
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+            self.tail = item
+        return
+
+    def traverse(self):
+        stack = []
+        ans = []
+        current = self.head
+        while current:
+            while current.data % 2 != 0:
+                if current.next is None:
+                    stack.append(current.data)
+                    current = current.next
+                    break
+                stack.append(current.data)
+                current = current.next
+
+
+            while (len(stack)):
+                ans.append(stack.pop())
+            if len(stack) == 0 :
+                if current is not None:
+                    if current.data % 2 == 0:
+                        ans.append(current.data)
+                        current = current.next
+        for i in range(len(ans)):
+            if ans[0] != 3 and i < 2:
+                print(ans[i]," ")
+            else:
+                print(ans[i])
 
 with open('input.pkl','rb') as inp:
     linkedlist = pickle.load(inp)
