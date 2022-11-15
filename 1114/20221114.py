9f9d1726895f1b6650b95362a110209fd3ed620c
Andreas00612 <113928052+Andreas00612@users.noreply.github.com>
2022-11-15 12:06:03 +0800

    Update 20221114.py

--- 1114/20221114.py
+++ 1114/20221114.py
@@ -1,4 +1,4 @@
-
+########<sol1>############
 def check_pow(input_num):
 
     if input_num == 1 :
@@ -8,3 +8,18 @@ def check_pow(input_num):
     return check_pow(input_num//2)
 
 print(check_pow(int(input())))
+
+
+########<sol2>############
+
+def power2(a):
+    if a==1:
+        return True
+    else:
+        if a % 2 !=0:
+            return False
+        else:
+            b=a/2 
+        return power2(b)
+  
+print(power2(int(input())))
